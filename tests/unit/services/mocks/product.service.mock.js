6212bcd6ae4aa3e72a6b7c2d6f18a070139b35c6
Martin Bachmann <martinpezbach@gmail.com>
2022-09-15 02:04:46 -0300

    test: add service layer add new product tests

--- tests/unit/services/mocks/product.service.mock.js
+++ tests/unit/services/mocks/product.service.mock.js
@@ -1,3 +1,4 @@
+// getAllProducts mock values
 const findAllReturn = [
   {
     "id": 1,
@@ -9,20 +10,25 @@ const findAllReturn = [
   }
 ]
 
-
+// getProductById mock values
 const findByIdSuccessReturn = {
   "id": 1,
   "name": "Martelo de Thor"
 }
 
-
 const findByIdErrorReturn = undefined;
 
 const getProductByIdErrorReturn = { type: 'NOT_FOUND', message: 'Product not found' }
+
+// addNewProduct mock values
+const insertSuccessReturn = {
+  "insertId": 4
+}
 
 module.exports = {
   findAllReturn,
   findByIdSuccessReturn,
   findByIdErrorReturn,
   getProductByIdErrorReturn,
+  insertSuccessReturn,
 }
--- tests/unit/services/product.service.test.js
+++ tests/unit/services/product.service.test.js
@@ -3,7 +3,7 @@ const sinon = require('sinon');
 
 const { productService } = require('../../../src/services');
 const { productModel } = require('../../../src/models');
-const { findAllReturn, findByIdSuccessReturn, findByIdErrorReturn, getProductByIdErrorReturn } = require('./mocks/product.service.mock');
+const { findAllReturn, findByIdSuccessReturn, findByIdErrorReturn, getProductByIdErrorReturn, insertSuccessReturn } = require('./mocks/product.service.mock');
 
 describe('Verifica a camada service de product', function () {
   afterEach(sinon.restore);
@@ -18,7 +18,7 @@ describe('Verifica a camada service de product', function () {
       expect(result.message).to.equal(findAllReturn);
     })
   })
-    describe('Verifica a função de buscar produto por id', function () {
+  describe('Verifica a função de buscar produto por id', function () {
     it('Caso de sucesso', async function () {
       sinon.stub(productModel, 'findById').resolves(findByIdSuccessReturn);
 
@@ -34,6 +34,16 @@ describe('Verifica a camada service de product', function () {
 
       expect(result.type).to.equal(getProductByIdErrorReturn.type);
       expect(result.message).to.equal(getProductByIdErrorReturn.message);
+    })
+  })
+  describe('Verifica a função de adicionar um novo produto', function () {
+    it('Caso de sucesso', async function () {
+      sinon.stub(productModel, 'insert').resolves(insertSuccessReturn);
+
+      const result = await productService.addNewProduct({ name: 'ProdutoX' });
+
+      expect(result.type).to.equal(null);
+      expect(result.message).to.equal({ name: 'ProdutoX', id: insertSuccessReturn.insertId });
     })
   })
 })
